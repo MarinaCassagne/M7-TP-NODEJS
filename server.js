f9c2a017be4099c7ec3ccb7d370270b06006d855
MarinaCassagne <marina_cassagne@yahoo.fr>
2025-11-14 17:41:02 +0100

    Remove MongoDB connection code
    Removed MongoDB connection code from server.js.

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 import 'dotenv/config';
 // console.log(`TEST ${process.env.SRC}`); // remove this after you've confirmed it is working
 
-// Connecter à MongoDB
-import mongoose from 'mongoose';
-mongoose.connect("mongodb+srv://marinacassagne_db_user:YPSQVFronLSXy6qr@tpnodejs.bviawx9.mongodb.net/");
 
 
 
